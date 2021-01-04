07a42cd09217029b56683232344ed146f994485a
Senroo <55132900+Senroo@users.noreply.github.com>
2021-01-04 14:52:20 +0100

    router

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,42 +1,39 @@
 <template>
   <div id="app">
-    <div class="header-composent">
-    <Header />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <div class="body">
-    <Welcome />
-    </div>
-    <footer>Le groupe de folie</footer>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Welcome from './components/welcome.vue'
-import Header from './components/header.vue'
+
 export default {
   name: 'App',
-  components: {
-    Welcome,
-    Header,
-  }
 }
 </script>
 
 <style>
-body{
-  margin: 3%;
-  background-color:#3C454C;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
+#nav {
+  padding: 30px;
 }
-.body{
-  margin-top: 10%;
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
-footer {
-  background: black;
-  color: white;
-  position: fixed;
-  bottom: 0;
-  width: 100vw;
-  margin: 0;
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
