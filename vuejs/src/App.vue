a623f869ce4187658df39705c9d43521a4e38b4e
Senroo <sengerromain75@gmail.com>
2021-01-04 12:31:25 +0100

    Header-test
    Création du composent header

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="app">
+    
+    <Header />
     <Welcome />
+    <footer>Le groupe de folie</footer>
   </div>
 </template>
 
 <script>
 import Welcome from './components/welcome.vue'
-
+import Header from './components/header.vue'
 export default {
   name: 'App',
   components: {
     Welcome,
+    Header,
   }
 }
 </script>
 
 <style>
+footer {
+  background: black;
+  color: white;
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
+  margin: 0;
+}
 </style>
---
+++ vuejs/src/components/header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <div class="header_container">
+      <div class="logo">
+        <img src="../assets/logo.png">
+      </div>
+      <div class="menu">
+        <p>Menu</p>
+        <p>Menu</p>
+        <p>Menu</p>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* Header style */
+.header_container {
+  display: flex;
+  justify-content: space-around;
+}
+
+.logo img {
+    width: 5    0%;
+}
+
+</style>
