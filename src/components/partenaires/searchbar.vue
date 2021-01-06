78c2ecd9aaa47ac102dca9f0303a188cbdd51e90
Lightuky <finalsmashganondorf@gmail.com>
2021-01-06 11:27:06 +0100

    fix Searchbar icon position

--- src/components/partenaires/searchbar.vue
+++ src/components/partenaires/searchbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="searchbar" class="mt-5 w-75 mx-auto">
+  <div id="searchbar" class="mt-5 w-75 mx-auto position-relative">
     <input type="search" placeholder="Trouver un partenaire" v-model="partSearchString" id="partSearchbar"
            class="form-control rounded text-center">
     <i class="fas fa-search position-absolute"></i>
