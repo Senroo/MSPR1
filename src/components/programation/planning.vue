4881d99a219c5874bb287995a237faccc1cffd7d
Senroo <55132900+Senroo@users.noreply.github.com>
2021-01-06 18:57:38 +0100

    programe + image artiste svg

--- src/components/programation/planning.vue
+++ src/components/programation/planning.vue
@@ -1,89 +1,99 @@
 <template>
-  <div>
-   <div id="app">
-  <select v-model="selectedType">
-    <option value="musique">Musique</option>
-    <option value="film">Film</option>
-    <option value="idk">IDK</option>
-    <option value="idk">Reset</option>
-  </select>
-  
-  <select v-model="selectedSize">
-    <option value="vendredi">Vendredi</option>
-    <option value="samedi">Samedi</option>
-    <option value="dimanche">Dimanche</option>
-  </select>
+  <div id="planning">
+    <select v-model="selectedType">
+      <option value="musique">Musique</option>
+      <option value="film">Film</option>
+      <option value="idk">IDK</option>
+      <option value="idk">Reset</option>
+    </select>
 
-  <button @click="shut()">Reset</button>
+    <select v-model="selectedSize">
+      <option value="vendredi">Vendredi</option>
+      <option value="samedi">Samedi</option>
+      <option value="dimanche">Dimanche</option>
+    </select>
 
-</div>
-  <ul>
-    <li v-for="(item, index) in computed_items" :key="index">
-      <span>{{ item.name }}</span> |
-      <span>{{ item.type }}</span> |
-      <span>{{ item.jour }}</span> |
-    </li>
-  </ul>
+    <button @click="shut()">Reset</button>
+    <div class="container-block mt-5">
+      <div
+        id="container-artist"
+        v-for="(item, index) in computed_items"
+        :key="index"
+      >
+        <img class="img mb-4 pr-2" :src="item.image" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return{
-      selectedType: '',
-      selectedSize: '',
-       items: [
-      {
-        name: 'Damso',
-        type: 'musique',
-        jour: 'vendredi'
-      },
-      {
-        name: 'Nekfeu',
-        type: 'idk',
-        jour: 'samedi'
-      },
-      {
-        name: 'Dua Lipa',
-        type: 'film',
-        jour: 'dimanche'
-      },
-      {
-        name: 'Ninho',
-        type: 'musique',
-        jour: 'vendredi'
-      }
-    ],
-    }
+    return {
+      selectedType: "",
+      selectedSize: "",
+      items: [
+        {
+          name: "Damso",
+          type: "musique",
+          jour: "vendredi",
+          image: require("@/assets/artists/damso.svg"),
+        },
+        {
+          name: "Ninho",
+          type: "idk",
+          jour: "samedi",
+          image: require("@/assets/artists/ninho.svg"),
+        },
+        {
+          name: "Dua Lipa",
+          type: "film",
+          jour: "dimanche",
+          image: require("@/assets/artists/dua lipa.svg"),
+        },
+        {
+          name: "Dj snake",
+          type: "musique",
+          jour: "vendredi",
+          image: require("@/assets/artists/dj snake.svg"),
+        },
+      ],
+    };
   },
   methods: {
-
-      shut(){
-        this.selectedType = undefined;
-        this.selectedSize = undefined;
-      }
+    shut() {
+      this.selectedType = undefined;
+      this.selectedSize = undefined;
+    },
   },
   computed: {
-    computed_items: function () {
-      let filterType= this.selectedType,
-          filterSize = this.selectedSize
-      return this.items.filter(function(item){
-        let filtered = true
-        if(filterType && filterType.length > 0){
-          filtered = item.type == filterType
+    computed_items: function() {
+      let filterType = this.selectedType,
+        filterSize = this.selectedSize;
+      return this.items.filter(function(item) {
+        let filtered = true;
+        if (filterType && filterType.length > 0) {
+          filtered = item.type == filterType;
         }
 
-        if(filtered){
-          if(filterSize && filterSize.length > 0){
-            filtered = item.jour == filterSize
+        if (filtered) {
+          if (filterSize && filterSize.length > 0) {
+            filtered = item.jour == filterSize;
           }
         }
-        return filtered
-      })
-    }
-  }
-}
+        return filtered;
+      });
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+.img {
+  width: 100%;
+}
+
+.container-block {
+  display: flex;
+}
+</style>
