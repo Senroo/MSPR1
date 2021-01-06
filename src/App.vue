2a9cc383137bddcac1af2369249ea2cfd1a5be02
Jifle <57390213+Jifle@users.noreply.github.com>
2021-01-06 12:34:08 +0100

    Added FAQ page on menu

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
         <li><router-link to="/about">About</router-link></li>
         <li><router-link to="/partenaire">Partenaires</router-link></li>
         <li><router-link to="/contact">Contact</router-link></li>
+        <li><router-link to="/Faq">FAQ</router-link></li>
       </ul>
     </Sidebar>
 
---
+++ src/views/Faq.vue
@@ -0,0 +1,42 @@
+<template>
+  <VueFaqAccordion :items="myItems"/>
+</template>
+
+<script>
+import VueFaqAccordion from "vue-faq-accordion";
+
+export default {
+  components: {
+    VueFaqAccordion
+  },
+  data() {
+    return {
+      myItems: [
+        {
+          title: "Combien de temps dure le festival ?",
+          value:
+            "Le festival dure sur 3 jours !",
+          category: "Le festival"
+        },
+        {
+          title: "How long is a day and year on Venus?",
+          value:
+            "Venus takes 224.7 Earth days to complete one orbit around the Sun.",
+          category: "Le lieu"
+        },
+        {
+          title: "What animal smells like popcorn?",
+          value: "Binturongs smell like popcorn.",
+          category: "Le lieu"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+</style>
