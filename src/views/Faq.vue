d294fc1894cb0f928864ccc9e8fb7a2ee4fa3e91
Jifle <57390213+Jifle@users.noreply.github.com>
2021-01-06 15:43:55 +0100

    Changement Css

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,6 +1,6 @@
 <template>
  <div id="faq" class="faq">
-    <h1 class="h1 text-center mt-4">Frequently asked question</h1>
+    <h1 class="h1 text-center mt-4 ">Frequently asked question</h1>
     
   <VueFaqAccordion :items="myItems"/> 
 
@@ -18,21 +18,26 @@ export default {
     return {
       myItems: [
         {
-          title: "Combien de temps dure le festival ?",
+          title: "Où se situe le festival ?",
           value:
-            "Le festival dure sur 3 jours !",
-          category: "Le festival"
+            "Le festival se déroule en région Parisienne et plus précisément 12 avenue de menthe la jolie, 75340!",
+          category: "Le lieu"
         },
         {
-          title: "How long is a day and year on Venus?",
+          title: "Comment je peux y accéder ?",
           value:
             "Venus takes 224.7 Earth days to complete one orbit around the Sun.",
           category: "Le lieu"
         },
         {
-          title: "What animal smells like popcorn?",
+          title: "Y'a t'il un plan du festival ?",
           value: "Binturongs smell like popcorn.",
           category: "Le lieu"
+        },
+        {
+          title: "Combien de temps dure le festival ?",
+          value: "Binturongs smell like popcorn.",
+          category: "Le festival"
         }
       ]
     };
@@ -48,6 +53,48 @@ export default {
 #faq{
 margin-left: auto;
     margin-right: auto;
-    width: 50em
+    width: 50em;
+border-color: red;
+
+}
+.faq__nav-item{
+active-color: #323A61;
+}
+#faq nav.faq__nav{
+  border-color: #323A61!important;
+  border-radius: 50px;
+}
+
+.faq__nav-item_active{
+  background-color: #323A61!important;
+  color: white!important;
+  border-top-left-radius: 50px;
+  border-bottom-left-radius: 50px;
+}
+.faq__nav:nth-child(2){
+border-top-right-radius: 50px!important;
+border-bottom-right-radius: 50px!important;
+}
+.accordion__item{
+  background-color: #323A61!important; 
+  border-radius: 50px;
+}
+.accordion__title{
+  color: white!important;
+}
+.accordion__value{
+  background-color: #C5CBE5;
+  border-radius: 100px;
+  width: auto;
+}
+accordion__toggle-button{
+  background-color: red!important;
+}
+.accordion{
+  border-color: blue!important;
+}
+h1{
+  color: #4043B4;
+  padding-bot: 1000px;
 }
 </style>
