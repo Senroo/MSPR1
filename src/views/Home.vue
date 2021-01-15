7b155a012b628a8312c65c336377ec0d46046607
Purefait <57390264+Purefait@users.noreply.github.com>
2021-01-15 17:49:24 +0100

    MAP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
     <img class="billet" src="@/assets/banner/banner.svg" alt="banner"/>
     <h2 class="titre-carte mt-4">Bienvenue dans la nation !</h2>
     <hr>
-    <img class="billet mt-2" src="@/assets/banner/Carte.svg" alt="carte"/>
+    <Map/>
   </div>
 </template>
 
@@ -13,21 +13,23 @@
 // @ is an alias to /src
 import Slider from "../components/artites/slider.vue";
 import News from "../components/menu/ImportantInfo.vue";
+import Map from "../components/bottom-navbar/map.vue"; 
+
 
 export default {
   name: 'Home',
   components: {
     Slider,
     News,
+    Map,
   }
 }
 </script>
 <style>
-.billet {
+.billet{
   width: 100%;
 }
-
-.titre-carte {
+.titre-carte{
   font-size: 1.5em;
 }
 
