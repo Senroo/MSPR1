5c9a240eee4ecc637c94a226efd7bda6d9acab2c
Lightuky <finalsmashganondorf@gmail.com>
2021-01-16 15:29:03 +0100

    artist carousel
    with boostrap vue

--- src/components/artites/slider.vue
+++ src/components/artites/slider.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="news">
-    <div
-      id="item-artistes"
-      class="d-flex flex-wrap justify-content-around text-center mt-5"
-    >
-      <div
-        id="container-artist"
-        v-for="(item, index) in items"
-        :key="index"
-        class="Item"
-      >
-        <img class="ItemImg mb-4 pr-2" :src="item.image" />
-      </div>
-    </div>
+    <b-carousel id="artistsSlider" class="mt-5" controls indicators :interval="4000" fade>
+      <b-carousel-slide v-for="i in Math.ceil(items.length / 3)" :key="i">
+        <template v-slot:img>
+          <b-card-group class="artistsSliderImgGroup d-flex">
+            <b-card v-for="item in items.slice((i - 1) * 3, i * 3)" :alt="item.name"
+                    :img-src="item.image" class="artistSliderImg border-0 m-2" :key="item.id" overlay>
+            </b-card>
+          </b-card-group>
+        </template>
+      </b-carousel-slide>
+    </b-carousel>
   </div>
 </template>
 
@@ -25,17 +23,17 @@ export default {
       items: [
         {
           id: 1,
-          name: "Damso",
-          type: "rap",
-          jour: "vendredi",
-          image: require("@/assets/artists/damso.svg"),
-        },
-        {
-          id: 2,
           name: "Ninho",
           type: "rap",
           jour: "samedi",
           image: require("@/assets/artists/ninho.svg"),
+        },
+        {
+          id: 2,
+          name: "Damso",
+          type: "rap",
+          jour: "vendredi",
+          image: require("@/assets/artists/damso.svg"),
         },
         {
           id: 3,
@@ -51,6 +49,20 @@ export default {
           jour: "vendredi",
           image: require("@/assets/artists/dj snake.svg"),
         },
+        {
+          id: 5,
+          name: "Tiesto",
+          type: "trance",
+          jour: "dimanche",
+          image: require("@/assets/artists/tiesto.svg"),
+        },
+        {
+          id: 6,
+          name: "Drake",
+          type: "hip-hop/rap",
+          jour: "samedi",
+          image: require("@/assets/artists/drake.svg"),
+        },
       ],
     };
   },
@@ -65,5 +77,15 @@ export default {
 
 .billet {
   width: 100%;
+}
+
+#artistsSlider .carousel-indicators li{
+  width: 0.9rem;
+  height: 0.9rem;
+}
+
+#artistsSlider .carousel-control-next span, #artistsSlider .carousel-control-prev span {
+  width: 5rem!important;
+  height: 5rem!important;
 }
 </style>
